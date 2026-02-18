4cb6805fac47da1d452def2b7ff2d564663eaaf4
Rio Weber <riodweber@gmail.com>
2026-02-18 13:36:12 -0500

    REMOVE Shadcn-vue;

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ import Home from "@/components/Home.vue"
 </script>
 
 <template>
-  <SidebarProvider>
-    <NavLeft />
-    <SidebarInset>
-      <NavTop />
-      <main>
-        <Home />
-      </main>
-    </SidebarInset>
+  <SidebarProvider class="flex flex-col min-h-screen">
+    <NavTop />
+    <div class="flex flex-1 overflow-hidden">
+      <NavLeft />
+      <SidebarInset>
+        <main>
+          <Home />
+        </main>
+      </SidebarInset>
+    </div>
   </SidebarProvider>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import tailwindcss from "@tailwindcss/vite"
 import vueDevTools from "vite-plugin-vue-devtools"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
@@ -10,7 +9,6 @@ import Components from "unplugin-vue-components/vite"
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
     vueDevTools(),
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
